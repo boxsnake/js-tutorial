5a8a575f3f190288662b3a084bced2912dfbed88
Alex Fang <deeno@qq.com>
2022-02-07 11:42:38 +0800

    feat: add i18n support

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,8 +3,30 @@ import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
   base: '/js-tutorial/',
-  lang: 'zh-CN',
-  title: 'Javascript 教程',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Javascript 教程',
+      description: '初学者的 Javascript 教程。'
+    },
+    '/en-US/': {
+      lang: 'en-US',
+      title: 'Javascript Tutorial',
+      description: 'A Javascript tutorial for starters.'
+    }
+  },
 
-  theme: '@vuepress/theme-default'
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    locales: {
+      '/': {
+        selectLanguageText: '选择语言',
+        selectLanguageName: '简体中文'
+      },
+      '/en-US/': {
+        selectLanguageText: 'Languages',
+        selectLanguageName: 'English'
+      }
+    }
+  }
 })
