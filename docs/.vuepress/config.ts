6594fc71dd15592c605d8e9eff06e2857114808d
Alex Fang <deeno@qq.com>
2022-02-07 17:30:08 +0800

    feat: add sidebar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
+import configs from './configs'
 
 export default defineUserConfig<DefaultThemeOptions>({
   base: '/js-tutorial/',
@@ -18,14 +19,17 @@ export default defineUserConfig<DefaultThemeOptions>({
 
   theme: '@vuepress/theme-default',
   themeConfig: {
+    sidebarDepth: 0,
     locales: {
       '/': {
         selectLanguageText: '选择语言',
-        selectLanguageName: '简体中文'
+        selectLanguageName: '简体中文',
+        sidebar: configs.sidebar.zhCN
       },
       '/en-US/': {
         selectLanguageText: 'Languages',
-        selectLanguageName: 'English'
+        selectLanguageName: 'English',
+        sidebar: configs.sidebar.enUS
       }
     }
   }
---
+++ docs/.vuepress/configs/sidebar/zh-CN.ts
@@ -0,0 +1,13 @@
+import type { SidebarConfig } from '@vuepress'
+import { prefixer } from '../utils/url'
+
+const urlPrefix : string = ''
+
+export const zhCN : SidebarConfig = [
+    prefixer(urlPrefix, '/README.md'),
+    {
+        text: '基础',
+        collapsible: true,
+        children: []
+    }
+]
