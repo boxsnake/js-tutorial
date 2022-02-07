0255e1d14575d3421ddee413a6e7e76e61b4d92c
Alex Fang <deeno@qq.com>
2022-02-07 10:19:26 +0800

    fix: change base router to match GitHub Pages url

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
+  base: '/js-tutorial/',
   lang: 'zh-CN',
   title: 'Javascript 教程',
 
